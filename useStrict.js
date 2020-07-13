d4bdac4ef05013a92cb2f92582ba4b30e03bb6d5
Teaca <tteaca@techteam.com>
2020-07-13 23:31:32 +0300

    The modern mode, use strict

---
+++ useStrict.js
@@ -0,0 +1,11 @@
+// "use strict" can be put at the top of a script 
+"use strict";
+var pi = 3.14;
+
+/*
+"use strict" can be put at the beginning of a function
+*/
+function useStrict(){
+    "use strict";
+    var pi = 3.14;
+}
