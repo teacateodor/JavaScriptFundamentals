f4602396ea370bea43217a97a31807957accbd0b
Teaca <tteaca@techteam.com>
2020-07-14 00:08:28 +0300

    Variables

---
+++ variables.js
@@ -0,0 +1,30 @@
+let variable;
+variable = 10;
+//alert(pi);
+
+let message = "hello";
+//alert(message);
+
+const NUMBER_PI = 3.14;
+//alert(pi);
+
+let a = 1,
+    b = "two",
+    c = 3;
+
+//the old var
+var x = 3.14;
+
+let hello = 'Hello world!';
+let message;
+message = hello;
+//alert(hello); // Hello world!
+//alert(message); // Hello world!
+
+// repeated 'let' leads to an error
+let message = "This";
+let message = "That"; // SyntaxError: 'message' has already been declared
+
+// when we need to pick a color
+const COLOR_ORANGE = "#FF7F00";
+let color = COLOR_ORANGE;
