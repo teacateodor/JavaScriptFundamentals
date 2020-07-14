e0cceaec49f91cad0ac7593efc90e62946c01bbd
Teaca <tteaca@techteam.com>
2020-07-14 09:34:53 +0300

    Type Conversion

--- alert.js
+++ alert.js
@@ -4,3 +4,10 @@ is
 multi-line
 comment
 */
+
+//promt
+let age = prompt('How old are you?', 100);
+alert(`You are ${age} years old!`);
+
+//confirm, result is true if OK is pressed and false otherwise
+let isBoss = confirm("Are you the boss?");
