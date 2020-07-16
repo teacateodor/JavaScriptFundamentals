725d4638f31c1938d4962924cafd6a96b6a88e27
Teaca <tteaca@techteam.com>
2020-07-16 18:13:09 +0300

    testing switch

--- Test/testswitch.js
+++ Test/testswitch.js
@@ -1,25 +1,8 @@
-let a = prompt("value");
-
-let switchValue;
-
-switch (a) {
-
-    case 1:
-        switchValue = 1;
-        break;
-
-    case 2:
-        switchValue = 2;
-        break;
-
-}
-
-alert(switchValue);
-
-
 describe("switch", function () {
 
     it("switch", function () {
-        assert.equal(a, switchValue);
+
+        assert.equal(caseNumber, number);
     });
+
 });
